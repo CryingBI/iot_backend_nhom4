697bd63cfe99677f79846a2937a3ab7f4bcff80c
Quang Anh <quanganhvp32401@gmail.com>
2023-01-08 20:18:36 +0700

    update controllers and requirement

--- controllers/customdevice.py
+++ controllers/customdevice.py
@@ -4,7 +4,7 @@ from utils.db import mysql
 from flask import jsonify
 from flask import flash, request
 
-
+#GET
 @app.route('/customdevice', methods=['GET'])
 def getAllCustomDevices():
     conn = None
@@ -24,6 +24,7 @@ def getAllCustomDevices():
 	    conn.close()
 
 
+#POST
 @app.route('/customdevice', methods=['POST'])
 def createCustomDevice():
     conn = None
@@ -38,7 +39,7 @@ def createCustomDevice():
         _created = _json['created_at']
         _updated = _json['updated_at']
         #validate
-        if _id and _roomID and _name and _param and _is_active and request.method == 'POST':
+        if _id!=None and _roomID!=None and _name!=None and _param!=None and _is_active!=None and request.method == 'POST':
             #save edited
             sql = "INSERT INTO customdevice VALUES(%s, %s, %s, %s, %s, %s, %s)"
             data = (_id, _roomID, _name, _param, _is_active, _created, _updated)
@@ -50,12 +51,74 @@ def createCustomDevice():
             res.status_code = 200
             return res
         else:
-            res = jsonify("Cannot create customed!")
+            res = jsonify("Cannot create customedevice!")
             return res
+    except Exception as e:
+        print(e)
+
+#Search one
+@app.route('/customdevice/<int:id>')
+def findCustomDevice(id):
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM customdevice WHERE id = %s", id)
+        row = cursor.fetchone()
+        res = jsonify(row)
+        res.status_code = 200
+        return res
     except Exception as e:
         print(e)
     finally:
         cursor.close()
         conn.close()
 
+#PUT
+@app.route('/update_dv/<int:id>', methods=['PUT'])
+def updateCustomDevice(id):
+    conn = None
+    cursor = None
+    try:
+        _json = request.json
+        _roomID = _json['roomID']
+        _name = _json['name']
+        _param = _json['param']
+        _is_active = _json['is_active']
 
+        if id!=None and _roomID!=None and _name!=None and _param!=None and _is_active!=None and request.method == 'PUT':
+            #update
+            sql = "UPDATE customdevice SET roomID=%s, name=%s, param=%s, is_active=%s WHERE id=%s"
+            data = (_roomID, _name, _param, _is_active, id)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            res = jsonify("Update customdevice successfully")
+            res.status_code = 200
+            return res
+        else:
+            res = jsonify("Update customdevice failed")
+            return res
+    except Exception as e:
+        print(e)
+
+#DELETE
+@app.route('/delele_dv/<int:id>', methods=['DELETE'])
+def deleteCustomDevices(id):
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM customdevice WHERE id=%s", id)
+        conn.commit()
+        res = jsonify("Delete customdevice successfully")
+        res.status_code = 200
+        return res
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
